43e4c427b11564ed7f1331391be36be44112e550
dana <157533000+Dana11a@users.noreply.github.com>
2026-04-27 16:54:44 +0300

    use

--- meras-backend/server.js
+++ meras-backend/server.js
@@ -15,7 +15,6 @@ connectDB();
 app.use(cors()); // allows React frontend to talk to this server
 app.use(express.json()); // allows the server to read JSON from request bodies
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/sessions", require("./routes/sessionRoutes"));
 
 app.get("/", (req, res) => {
   res.send("Meras backend NEW TEST");
@@ -26,8 +25,7 @@ app.use("/api/challenges", require("./routes/challengeRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/submissions", submissionRoutes);
 app.use("/uploads", express.static("uploads"));
-
-
+app.use("/api/sessions", require("./routes/sessionRoutes"));
 
 const PORT = process.env.PORT || 5000;
 
